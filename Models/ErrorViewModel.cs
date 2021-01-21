f1b09c62b13984fe6853c1a4ab472a2b7803eb21
S Bose <bose2007s@gmail.com>
2021-01-21 20:11:04 +0530

    test commit for CI

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Projectdotnet1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
